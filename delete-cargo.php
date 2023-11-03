6108f46c6bad772789d91d80de2a592079772681
DelisG <delisemarciano@gmail.com>
2023-11-03 14:41:43 -0300

    ➖ Exclusão de Cargo

---
+++ delete-cargo.php
@@ -0,0 +1,70 @@
+<?php
+require 'banco.php';
+
+$id = 0;
+
+if(!empty($_GET['id']))
+{
+    $id = $_REQUEST['id'];
+}
+
+if(!empty($_POST))
+{
+    $id = $_POST['id'];
+
+    //Delete do banco:
+    $pdo = Banco::conectar();
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "DELETE FROM funcao where id = ?";
+    $q = $pdo->prepare($sql);
+    $q->execute(array($id));
+    Banco::desconectar();
+    header("Location: cargos.php");
+}
+?>
+
+    <!DOCTYPE html>
+    <html lang="pt-br">
+
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="assets/style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <title>Excluir Cargo</title>
+    </head>
+
+    <body>
+        <div class="container-fluid h-100">
+        <div class="row h-100">
+            <div class="col-lg-2 bg-dark">    
+                <a href="index.php"><h5 class="text-light text-center py-4">Sistema Empresarial Unificado</h5></a>
+                <div class="d-flex flex-column my-3">
+                    <a href="index.php" class="mb-3 btn btn-dark text-left"><i class="fa-solid fa-house-chimney mr-2"></i>Inicio</a>
+                    <a href="funcionarios.php" class="mb-3 btn btn-dark text-left"><i class="fa-solid fa-user-group mr-2"></i>Listar Funcionários</button></a>
+                    <a href="create-funcionario.php" class="mb-3 btn btn-dark text-left"><i class="fa-solid fa-user-plus mr-2"></i>Adicionar Funcionário</button></a>
+                    <a href="cargos.php" class="mb-3 btn btn-dark text-left"><i class="fa-solid fa-file mr-2"></i>Listar Cargos</button></a>
+                    <a href="create-cargo.php" class="mb-3 btn btn-dark text-left"><i class="fa-solid fa-file-circle-plus mr-2"></i>Adicionar Cargo</button></a>                
+                </div>                 
+            </div>
+            <div class="col-md mt-4">
+                    <h5 class="well">Excluir Cargo</h5>
+                    
+                    <form class="form-horizontal" action="delete-cargo.php" method="post">
+                        <input type="hidden" name="id" value="<?php echo $id;?>" />
+                        <div class="alert alert-danger"> Deseja excluir o Cargo?
+                        </div>
+                        <div class="form actions">
+                            <button type="submit" class="btn btn-danger">Sim</button>
+                            <a href="cargos.php" type="btn" class="btn btn-outline-danger">Não</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+
+    </body>
+
+    </html>
